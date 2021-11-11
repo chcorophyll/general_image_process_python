06004f108e91b8fa0dd4964cf0f74e1af76928b1
chcorophyll <chcorophyll@gmail.com>
2021-11-11 11:14:54 +0800

    descriptor done

--- Sift.py
+++ Sift.py
@@ -290,7 +290,92 @@ def generate_descriptors(key_points, gaussian_images, window_width=4,
                          num_bins=8, scale_multiplier=3, descriptor_max_value=0.2):
     descriptors = []
     for key_point in key_points:
-        
+        octave, layer, scale = unpack_octave(key_point)
+        gaussian_image = gaussian_images[octave + 1, layer]
+        num_rows, num_cols = gaussian_image.shape
+        point = round(scale * np.array(key_point.pt)).astype("int")
+        bins_per_degree = num_bins / 360.0
+        angle = 360.0 - key_point.angle
+        cos_angle = np.cos(np.deg2rad(angle))
+        sin_angle = np.sin(np.deg2rad(angle))
+        weight_multiplier = -0.5 / ((0.5 * window_width) ** 2)
+        row_bin_list = []
+        col_bin_list = []
+        magnitude_list = []
+        orientation_bin_list = []
+        histogram_tensor = np.zeros((window_width + 2, window_width + 2, num_bins))
+        hist_width = scale_multiplier * 0.5 * scale * key_point.size
+        half_width = int(round(hist_width * np.sqrt(2) * (window_width + 1) * 0.5))
+        half_width = int(min(half_width, np.sqrt(num_rows ** 2 + num_cols ** 2)))
+        for row in range(-half_width, half_width + 1):
+            for col in range(-half_width, half_width + 1):
+                row_rotation = col * sin_angle + row * cos_angle
+                col_rotation = col * cos_angle - row * sin_angle
+                row_bin = (row_rotation / hist_width) + 0.5 * window_width - 0.5
+                col_bin = (col_rotation / hist_width) + 0.5 * window_width - 0.5
+                if row_bin > -1 and row_bin < window_width and col_bin > -1 and col_bin < window_width:
+                    window_row = int(round(point[1] + row))
+                    window_col = int(round(point[0] + col))
+                    if 0 < window_row < num_rows - 1 and 0 < window_col < num_cols - 1:
+                        dx = gaussian_image[window_row, window_col + 1] - gaussian_image[window_row, window_col - 1]
+                        dy = gaussian_image[window_row - 1, window_col] - gaussian_image[window_row + 1, window_col]
+                        gradient_magnitude = np.sqrt(dx * dx + dy * dy)
+                        gradient_orientation = np.rad2deg(np.arctan2(dy, dx)) % 360
+                        weight = np.exp(weight_multiplier * ((row_rotation / hist_width) ** 2 +
+                                                              (col_rotation / hist_width) ** 2))
+                        row_bin_list.append(row_bin)
+                        col_bin_list.append(col_bin)
+                        magnitude_list.append(weight * gradient_magnitude)
+                        orientation_bin_list.append((gradient_orientation - angle) * bins_per_degree)
+        for row_bin, col_bin, magnitude, orientation_bin in zip(row_bin_list,
+                                                                col_bin_list,
+                                                                magnitude_list,
+                                                                orientation_bin_list):
+            row_bin_floor, col_bin_floor, orientation_bin_floor = np.floor([row_bin, col_bin,
+                                                                            orientation_bin]).astype("int")
+            row_fraction, col_fraction, orientation_fraction = row_bin - row_bin_floor, \
+                                                               col_bin - col_bin_floor, \
+                                                               orientation_bin - orientation_bin_floor
+            if orientation_bin_floor < 0:
+                orientation_bin_floor += num_bins
+            if orientation_bin_floor >= num_bins:
+                orientation_bin_floor -= num_bins
+            c1 = magnitude * row_fraction
+            c0 = magnitude * (1 - row_fraction)
+            c11 = c1 * col_fraction
+            c10 = c1 * (1 - col_fraction)
+            c01 = c0 * col_fraction
+            c00 = c0 * (1 - col_fraction)
+            c111 = c11 * orientation_fraction
+            c110 = c11 * (1 - orientation_fraction)
+            c101 = c10 * orientation_fraction
+            c100 = c10 * (1 - orientation_fraction)
+            c011 = c01 * orientation_fraction
+            c010 = c01 * (1 - orientation_fraction)
+            c001 = c00 * orientation_fraction
+            c000 = c00 * (1 - orientation_fraction)
+            histogram_tensor[row_bin_floor+1, col_bin_floor+1, orientation_bin_floor] += c000
+            histogram_tensor[row_bin_floor + 1, col_bin_floor + 1, (orientation_bin_floor + 1) % num_bins] += c001
+            histogram_tensor[row_bin_floor + 1, col_bin_floor + 2, orientation_bin_floor] += c010
+            histogram_tensor[row_bin_floor + 1, col_bin_floor + 2, (orientation_bin_floor + 1) % num_bins] += c011
+            histogram_tensor[row_bin_floor + 2, col_bin_floor + 1, orientation_bin_floor] += c100
+            histogram_tensor[row_bin_floor + 2, col_bin_floor + 1, (orientation_bin_floor + 1) % num_bins] += c101
+            histogram_tensor[row_bin_floor + 2, col_bin_floor + 2, orientation_bin_floor] += c110
+            histogram_tensor[row_bin_floor + 2, col_bin_floor + 2, (orientation_bin_floor + 1) % num_bins] += c111
+
+        descriptor_vector = histogram_tensor[1:-1, 1:-1, :].flatten()
+        threshold = np.linalg.norm(descriptor_vector) * descriptor_max_value
+        descriptor_vector[descriptor_vector > threshold] = threshold
+        descriptor_vector /= np.max(np.linalg.norm(descriptor_vector), FLOAT_TOLERANCE)
+        descriptor_vector = round(512 * descriptor_vector)
+        descriptor_vector[descriptor_vector < 0] = 0
+        descriptor_vector[descriptor_vector > 255] = 255
+        descriptors.append(descriptor_vector)
+    return descriptors
+
+
+
+
 
 
 
