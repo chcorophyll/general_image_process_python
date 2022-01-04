2335d40af8db86faa4c93bc67e30f16baff4ccaf
chcorophyll <chcorophyll@gmail.com>
2022-01-04 19:28:55 +0800

    lbp done

--- LocalBinaryPattern.py
+++ LocalBinaryPattern.py
@@ -31,7 +31,7 @@ def coord_map(dimensions, coordinate, mode):
         if coordinate < 0:
             coordinate = -coordinate - 1
         if coordinate > max_coordinate:
-            if (coordinate / dimensions) % 2 != 0:
+            if (coordinate // dimensions) % 2 != 0:
                 return max_coordinate - (coordinate % dimensions)
             else:
                 return coordinate % dimensions
@@ -49,12 +49,12 @@ def coord_map(dimensions, coordinate, mode):
         if dimensions == 1:
             return 0
         elif coordinate < 0:
-            if (-coordinate / max_coordinate) % 2 != 0:
+            if (-coordinate // max_coordinate) % 2 != 0:
                 return max_coordinate - (-coordinate % max_coordinate)
             else:
                 return -coordinate % max_coordinate
         elif coordinate > max_coordinate:
-            if (coordinate / max_coordinate) % 2 != 0:
+            if (coordinate // max_coordinate) % 2 != 0:
                 return max_coordinate - (coordinate % max_coordinate)
             else:
                 return coordinate % max_coordinate
@@ -90,6 +90,10 @@ def _bilinear_interpolation(image, rows, cols, row, col, mode="C", constant_valu
     return out
 
 
+def _bit_rotate_right(value, length):
+    return (value >> 1) | ((value & 1) << (length - 1))
+
+
 def local_binary_pattern(image, points_number, radius, method="D"):
     weights = 2 ** np.arange(points_number)
     rr = - radius * np.sin(2 * np.pi * np.arange(points_number) / points_number)
@@ -107,3 +111,70 @@ def local_binary_pattern(image, points_number, radius, method="D"):
         for c in range(cols):
             for i in range(points_number):
                 texture[i] = _bilinear_interpolation(image, rows, cols, r+rp[i], c+cp[i], "C", 0)
+            for i in range(points_number):
+                if texture[i] - image[r][c] >= 0:
+                    signed_texture[i] = 1
+                else:
+                    signed_texture[i] = 0
+            lbp = 0
+            if method == "V":
+                sum_ = 0
+                var_ = 0
+                for i in range(points_number):
+                    sum_ += texture[i]
+                    var_ += texture[i] * texture[i]
+                lbp = (var_ - (sum_ * sum_) / points_number) / points_number # var = E(x^2) - (E(x))^2
+            elif method == "U" or method == "N":
+                changes = 0
+                for i in range(points_number-1):
+                    changes += (signed_texture[i] - signed_texture[i+1]) != 0
+                if method == "N":
+                    if changes <= 2:
+                        n_ones = 0
+                        first_one = -1
+                        first_zero = -1
+                        for i in range(points_number):
+                            if signed_texture[i]:
+                                n_ones += 1
+                                if first_one == -1:
+                                    first_one = i
+                            else:
+                                if first_zero == -1:
+                                    first_zero = i
+                        if n_ones == 0:
+                            lbp = 0
+                        elif n_ones == points_number:
+                            lbp = points_number * (points_number - 1) + 1
+                        else:
+                            if first_one == 0:
+                                rot_index = n_ones - first_zero
+                            else:
+                                rot_index = points_number - first_one
+                            lbp = 1 + (n_ones - 1) * points_number + rot_index  # 起点是xx00 ？？
+                    else:
+                        lbp = points_number * (points_number - 1) + 2
+                else:
+                    if changes <= 2:
+                        for i in range(points_number):
+                            lbp += signed_texture[i]
+                    else:
+                        lbp = points_number + 1
+            else:
+                # method == "default"
+                for i in range(points_number):
+                    lbp += signed_texture[i] * weights[i]
+                if method == "R":
+                    rotation_chain[0] = lbp
+                    for i in range(1, points_number):
+                        rotation_chain[i] = _bit_rotate_right(rotation_chain[i-1], points_number)
+                    lbp = rotation_chain[0]
+                    for i in range(1, points_number):
+                        lbp = min(lbp, rotation_chain[i])
+            output[r, c] = lbp
+    return output
+
+
+
+
+
+
